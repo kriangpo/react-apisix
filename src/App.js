43d89a6e2a5241ac95c3a025b04df3432126f092
superuoa <superuoa@gmail.com>
2025-09-08 05:47:36 +0700

    add credentials

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = () => {
     try {
       const response = await fetch(API_URL, {
         method: 'GET',
+        credentials: "include"
 
       });
       console.log('Final URL:', response.url);
