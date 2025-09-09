0bddf83202d80ead2a3e31aff90e53f700b6dacb
superuoa <superuoa@gmail.com>
2025-09-10 01:10:40 +0700

    update url backend

--- src/components/Index.js
+++ src/components/Index.js
@@ -23,7 +23,7 @@ const Index = () => {
 
   const fetchProtectedData = async (token) => {
     try {
-      const response = await fetch('https://docker2.devops.esc.yipintsoigroup.com/test/api', {
+      const response = await fetch('https://docker2.devops.esc.yipintsoigroup.com/yip-cert-api/courses/', {
         headers: {
           'Authorization': `Bearer ${token}`
         }
